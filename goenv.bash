e1913505320cb1344e6c6c3cc58e3455bc4845ba
jinut2 <jinu.t.92@gmail.com>
2016-12-28 17:44:10 +0530

    added goenv cmd.

--- goenv.bash
+++ goenv.bash
@@ -5,7 +5,7 @@ lsgoenv(){
 
 mkgoenv(){
     if [ -z "$1" ]; then
-        echo "no valid project name."
+        echo "Add a valid project name."
         echo "eg.   mkgoenv project-name"
     else
         if [ -f ~/.goenv/projects/$1 ]; then
@@ -38,6 +38,25 @@ rmgoenv(){
             export GOPATH=""
         else
             echo "project '$1' not found."
+            lsgoenv
+        fi
+    fi
+}
+
+goenv(){
+    if [ -z "$1" ]; then
+        echo "Add a valid project name."
+        echo "eg.   goenv project-name"
+        lsgoenv
+    else
+        if [ -f ~/.goenv/projects/$1 ]; then
+            source ~/.goenv/projects/$1
+            echo "Loading project '$PROJECT_PATH'"
+            export GOPATH=$PROJECT_PATH/
+            cd $GOPATH/src
+        else
+            echo "project '$1' not found."
+            lsgoenv
         fi
     fi
 }
